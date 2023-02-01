82a62f264fb01b5eddeb4b00b54c0fa14bb951cf
nabil-slash <nabilafsan235@gmail.com>
2023-02-01 10:52:06 +0600

    2nd commite

---
+++ TestMVC/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestMVC.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
